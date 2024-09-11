4dd666c49e48f7c3772528ba8ee8a40e96a792f5
LouiseChili <chililove@users.noreply.github.com>
2024-09-11 16:56:52 +0200

    connect with console app and create exporting to csv file functionality

--- EmployeeManagementSystem/EmployeeManagement.ConsoleApp/Program.cs
+++ EmployeeManagementSystem/EmployeeManagement.ConsoleApp/Program.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.NetworkInformation;
+using EmployeeManagement.ConsoleApp;
+
 
 public class Program
 {
@@ -12,32 +14,43 @@ public class Program
 
     public static async Task Main(string[] args)
     {
-try{
-    string apiUrl = "http://localhost:5050/api/employess";
+        try
+        {
+            string apiUrl = "http://localhost:5050/api/employess";
 
-    Console.WriteLine("Getting data from my web api for employees");
+            Console.WriteLine("Getting data from my web api for employees");
 
-    // GET
-    var employees = await client.GetFromJsonAsync<List<Employee>>(apiUrl);
+            // GET
+            var employees = await client.GetFromJsonAsync<List<Employee>>(apiUrl);
 
-    if(employees == null || employees.Count == 0)
+            if (employees == null || employees.Count == 0)
+            {
+                Console.WriteLine("No data of employees are found.");
+                return;
+            }
+
+            Console.WriteLine("Succesfully got employee data grabbed/fetched");
+
+            //Export CSV file
+            // ExportToCsv(employees);
+
+            Console.WriteLine("Exporting data from employees to csv file");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"There has been an error: {ex.Message}");
+        }
+    }
+    private static void ExportToCsv(List<Employee> employees)
     {
-        Console.WriteLine("No data of employees are found.");
-        return;
+        using (var writer = new StreamWriter("employees.csv"))
+        {
+            writer.WriteLine("Id,Name,Position,Salary");
+            foreach (var employee in employees)
+            {
+                writer.WriteLine($"{employee.Id}, {employee.Name}, {employee.Position}, {employee.Salary}");
+            }
+        }
     }
-
-    Console.WriteLine("Succesfully got employee data grabbed/fetched");
-
-    //Export CSV file
-   // ExportToCsv(employees);
-
-    Console.WriteLine("Exporting data from employees to csv file");
-}
-catch(Exception ex)
-{
-Console.WriteLine($"There has been an error: {ex.Message}");
-}
-    }
-
 
 }
